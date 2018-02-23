9ec93003cbbb9744e41a25a3c0a05b500ee93cae
Andrei Nagacevschi <andrei.nagacevschi@osf-commerce.com>
2018-02-23 21:21:48 +0200

    Adding an echo server for testing.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ extern crate futures;
 use futures::future::Future;
 use hyper::header::ContentLength;
 use hyper::server::{Http, Request, Response, Service};
+use hyper::{Method, StatusCode};
 
-struct HelloWorld;
+struct Echo;
 
 const PHRASE: &'static str = "Hello World!";
 
-impl Service for HelloWorld {
+impl Service for Echo {
     // boilerplate hooking up hyper's server types
     type Request = Request;
     type Response = Response;
@@ -18,20 +19,30 @@ impl Service for HelloWorld {
     // resolve to. This can change to whatever Future you need.
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
+    fn call(&self, req: Request) -> Self::Future {
         // We're currently ignoring the Request
         // And returning and 'ok' Future, which means it's ready
         // immediately, and build a Response with the 'PHRASE' body
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_body(PHRASE)
-        ))
+        let mut response = Response::new();
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                response.set_body("Try POSTing data to /echo");
+            },
+            (&Method::Post, "/echo") => {
+                response.set_body(req.body());
+            },
+            _=> {
+                response.set_body(String::from(req.path()));
+                response.set_status(StatusCode::NotFound);
+            }
+        }
+        Box::new(futures::future::ok(response))
     }
 }
 
 fn main() {
     let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
     server.run().unwrap();
 }
