becf49117a48d66ac30759bb8fb6f19eaffbd78f
Andrei Nagacevschi <andrei.nagacevschi@osf-commerce.com>
2018-03-04 18:08:40 +0200

    Ading new route implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 extern crate hyper;
 extern crate futures;
 
-use futures::future::Future;
-use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service};
-use hyper::{Method, StatusCode};
+// use futures::future::Future;
+// use hyper::header::ContentLength;
+// use hyper::server::{Http, Request, Response, Service};
+// use hyper::{Method, StatusCode};
 
+mod server;
 mod router;
 
 fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(router::Router)).unwrap();
-    server.run().unwrap();
+    // // let Router = router::Router::new();
+    // let ref custom_router = router::Router {
+    //     routes: vec!("test".to_string())
+    // };
+    // let addr = "127.0.0.1:3000".parse().unwrap();
+    // // let server = Http::new().bind(&addr, || Ok(router::Router{routes: vec!("test".to_string())})).unwrap();
+    // let server = Http::new().bind(&addr, || Ok(router::Router {routes: vec!("test".to_string())})).unwrap();
+    // server.run().unwrap();
+
+    server::Server::new().http();
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,15 +1,17 @@
 use futures;
-use futures::future::Future;
 use hyper;
+use futures::future::Future;
 use hyper::header::ContentLength;
 use hyper::server::{Http, Request, Response, Service};
 use hyper::{Method, StatusCode};
 
-pub struct Router {
-    pub routes: Vec<String>
+// use router;
+struct RustyService;
+pub struct Server {
+    routes: 
 }
 
-impl Service for Router {
+impl Service for RustyService {
     // boilerplate hooking up hyper's server types
     type Request = Request;
     type Response = Response;
@@ -19,17 +21,20 @@ impl Service for Router {
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
-        println!("first rout is: {0}", self.routes.len());
+        // println!("first rout is: {0}", self.routes.len());
         let mut response = Response::new();
         response.set_body(req.path().to_string());
         Box::new(futures::future::ok(response))
     }
 }
 
-impl Router {
-    pub fn new() -> Router {
-        Router {
-            routes: vec!("test".to_string(), "newtest".to_string())
-        }
+impl Server {
+    pub fn new(routes) -> Server {
+        Server
+    }
+
+    pub fn http(&self) -> () {
+        let addr = "127.0.0.1:3000".parse().unwrap();
+        Http::new().bind(&addr, || Ok(RustyService)).unwrap().run().unwrap();
     }
 }
