2a8c2dc199528634b55539e67d80520ecbb35ef4
Andrei Nagacevschi <andrei.nagacevschi@osf-commerce.com>
2018-05-25 19:26:57 +0300

    Testing redirection in hyper.

--- src/controllers/admin.rs
+++ src/controllers/admin.rs
@@ -1,12 +1,20 @@
 use base::Handler;
 use base::Request;
 use base::Response;
+use hyper::{StatusCode, header};
 
 pub struct Index;
 
 impl Handler for Index {
     fn handle(&self, _req: &mut Request) -> Response {
         let mut response = Response::new();
+
+        if true {
+            response.set_status(StatusCode::Found);
+            response.headers_mut().set(header::Location::new("/lala"));
+            return response;
+        }
+
         response.set_body("admin test body".to_string());
         response
     }
